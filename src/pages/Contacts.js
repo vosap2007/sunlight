60b1689cd8c8ebec44557319cb9532d7ada1ec14
Volodymyr Udovychenko <vosap2007@i.ua>
2021-05-27 00:34:32 +0300

    Добавляет контакты

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
 
 export default class Contacts extends Component {
     render() {
         return (
-            <div>
-              Hello Contacts
-            </div>
+            <Container className='text-center'>
+              <h1 className='mt-3'>Зв'яжіться з нами</h1>
+              <h4 className='mt-3'>моб: 000 000-00-00</h4>
+              <h4>email: aaa@aaaa.aa</h4>
+              <h4 className='mt-3'>м. Київ, метро Позняки,
+              <br/> вул. Лариси Руденко 6а, 3 пов
+              <br/> (зі сторони школи)
+                  <br/> 
+                  <div className='mt-3'>Пн-Пт 10 - 19, Сб 11 - 17</div>
+             </h4>
+              <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVlVTTYqhKz9XF8Ot1Qn_2zFJ8f4odEBJumA&usqp=CAU' />
+            </Container>
         )
     }
 }
